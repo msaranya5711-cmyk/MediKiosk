1711a145de8d06192df02c83f5570a1f4311e379
msaranya5711-cmyk <msaranya.5711@gmail.com>
2026-09-13 23:56:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY
 });
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
